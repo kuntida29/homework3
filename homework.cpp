065cc398fea4ae1bbb79868c1bb3c52151814e06
Kuntida Kijprasong <68277300+kuntida29@users.noreply.github.com>
2020-08-04 13:45:34 +0700

    Add files via upload

---
+++ homework.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int first,second,third,mid_term,final;
+	cout <<"=============QUIZZES============" <<endl;
+	cout <<"Enter first quizz (10) :";
+	cin >> first;
+	cout <<"Enter second quizz (10) :";
+	cin >> second;
+	cout <<"Enter third quizz (10) :";
+	cin >> third;
+	cout <<"=============MID-TERM============" <<endl;
+	cout <<"Enter mid-term (40) :";
+	cin >> mid_term;
+	cout <<"=============FINAL============" <<endl;
+	cout <<"Enter final (50) :";
+	cin >> final;
+	float QuizzTotal , sum , Total ;
+	sum = first + second + third ;
+	QuizzTotal = sum / 3 ;
+	cout << "Quizz Total : " << QuizzTotal <<endl;
+	cout << "Mid term : " << mid_term << endl;
+	cout << "Final : " << final <<endl;
+	Total = QuizzTotal + mid_term + final;
+	cout << "Total : " << Total <<endl;
+	cout << "Our Score is " << ((Total) >=50 ? "PASS" : "FAIL") <<endl;
+
+	system("pause");
+	return (0);
+}
